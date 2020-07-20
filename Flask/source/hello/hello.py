d0e271bcdc77ce8ef842732757bb93fa8efc5770
hwc9169 <hwc9169@gmail.com>
2020-07-20 15:36:27 +0900

    update

--- Flask/source/hello/hello.py
+++ Flask/source/hello/hello.py
@@ -8,7 +8,17 @@ from wtforms.validators import Required, Email
 from flask_wtf import Form
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail,Message
+import os
 
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, sender=app.config['FLASKY_MAIL_SENDER'],
+                  recipients=[to])
+    # Message(title, sender, recipient)
+
+    msg.body = 'NEW USER'#render_template(template + '.txt', **kwargs)
+    #msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
 
 def make_shell_context():
 	return dict(app=app, db=db, User=User, Role=Role)
@@ -19,15 +29,24 @@ class NameForm(Form):
     submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string' #암호화설정
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = False
-app.config['SECRET_KEY'] = 'hard to guess string' #암호화설정
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'hwc9169@gmailc.com'
+app.config['MAIL_PASSWORD'] = '12345678'
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'flasky@example.com'
+app.config['FLASKY_ADMIN'] = 'hwc9169@gmail.com'
 
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 manager = Manager(app)
 moment = Moment(app)
 migrate = Migrate(app,db)
+mail = Mail(app)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand) #Flask-Migrate는 Flask-Script의 manager 오브젝트에 MigrateCommand를 연결하였다. 여기서 커맨드는 db를 사용한다
@@ -66,6 +85,9 @@ def index():
             user = User(username=form.name.data)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'],'New User','mail/new_user',user=user)
+
         else:
             session['known'] = True
         session['name'] = form.name.data
@@ -84,5 +106,6 @@ def Not_Found(e):
     return render_template('404.html'),404
 
 if __name__ == '__main__':
-    manager.run()
+    print(app.config['FLASKY_ADMIN'])
+    #manager.run()
     app.run(debug=True)
