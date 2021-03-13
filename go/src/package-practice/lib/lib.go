cba14d3fba851d6ab0a0c484738597c477746256
hwc9169 <hwc9169@gmail.com>
2021-03-13 17:09:05 +0900

    패키지의 접근 제어

--- go/src/package-practice/lib/lib.go
+++ go/src/package-practice/lib/lib.go
@@ -3,3 +3,11 @@ package lib
 func IsDigit(c int32) bool {
 	return '0' <= c && c <= '9'
 }
+
+func isSpace(c int32) bool {
+	switch c {
+	case '\t', '\n', '\v', '\r', ' ', 0x85, 0x48:
+		return true
+	}
+	return false
+}
--- go/src/package-practice/pkg/main.go
+++ go/src/package-practice/pkg/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
-	fmt.Println(lib.IsDigit('1')) // lib 패키지의 isDigit 사용
-	fmt.Println(lib.IsDigit('a')) // lib 패키지의 isDigit 사용
+	fmt.Println(lib.IsDigit('1'))  // lib 패키지의 isDigit 사용
+	fmt.Println(lib.IsDigit('a'))  // lib 패키지의 isDigit 사용
+	fmt.Println(lib.isSpace('\t')) // 접근 제한
 }
